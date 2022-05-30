a1f30c344d33bcfc2e786336cdbfa795544328fb
Usama <um3121997@gmail.com>
2022-05-30 11:24:32 -0500

    hydration error reolved

--- component/Card.js
+++ component/Card.js
@@ -2,58 +2,53 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 //import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea,  Button } from '@mui/material';
-import Image from 'next/image';
-import Link from 'next/link'
-import Detail from '../component/Detail'
-import { useRouter } from 'next/router';
+import Typography from "@mui/material/Typography";
+import { CardActionArea, Button, CardActions } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import Detail from "../component/Detail";
+import { useRouter } from "next/router";
 
-
-export default function ActionAreaCard({product,viewProduct,addToCartHandler}) {
-    
-    
-  
-    return (
+export default function ActionAreaCard({
+  product,
+  viewProduct,
+  addToCartHandler,
+}) {
+  return (
     <>
-    <div style={{display:'flex', flexWrap:'wrap'}}>
-       
-      {
-          product.map(item=>(
-            <Card >
-            <CardActionArea>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {product.map((item) => (
+          <Card>
             <Image
-                                    src={item.imgdata}
-                                    alt='shirt'
-                                    width={100}
-                                    height={300}
-                                  ></Image>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                 {item.rname}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {item.somedata}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {item.price}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <Button key={item.id} onClick={e=>viewProduct(item)}>View Product</Button>  
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  <Button key={item.id} onClick={e=>addToCartHandler(item)}>Add To Cart</Button>  
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            
+              src={item.imgdata}
+              alt="shirt"
+              width={100}
+              height={300}
+            ></Image>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {item.rname}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {item.somedata}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {item.price}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                <Button key={item.id} onClick={(e) => viewProduct(item)}>
+                  View Product
+                </Button>
+              </Typography>
+              <CardActions>
+                <Button key={item.id} onClick={(e) => addToCartHandler(item)}>
+                  Add To Cart
+                </Button>
+              </CardActions>
+            </CardContent>
           </Card>
-          ))
-      }  
-    
-    
-    </div>
-    </>    
-    
+        ))}
+      </div>
+    </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
-import { Provider } from 'react-redux'
-import { store } from '../app/store'
-import '../styles/globals.css'
-import { Provider as AuthProvider } from 'next-auth/client'
-import { ThemeProvider } from '@mui/material'
-import { theme } from '../theme'
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../theme";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <AuthProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session}>
       <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
         </ThemeProvider>
       </Provider>
-    </AuthProvider>
-  )
-}
+    </SessionProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
