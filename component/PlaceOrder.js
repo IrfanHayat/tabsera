a8a0a9a4d601c4c8b569e56bfb923d15bda1d97b
IrfanHayat <irfanhayat55@gmail.com>
2022-05-31 12:20:21 +0500

    placeorder working

--- component/PlaceOrder.js
+++ component/PlaceOrder.js
@@ -20,70 +20,12 @@ import {
 } from "@mui/material";
 //import axios from 'axios';
 import { useRouter, withRouter } from "next/router";
-import useStyles from "../utils/styles";
+
 import CheckoutWizard from "../component/CheckoutWizard";
-import { clearBasket } from "../slice/basketSlice";
-//import Cookies from 'js-cookie';
-import { useSelector, useDispatch } from "react-redux";
-import localStorage from "localStorage";
 
-function PlaceOrder() {
-  const classes = useStyles();
-  const router = useRouter();
-  const { dispatch } = useDispatch();
-  const {
-    cart: { cartItems, shippingAddress, paymentMethod },
-  } = useSelector(state => state.basket);
-  const round2 = num => Math.round(num * 100 + Number.EPSILON) / 100; // 123.456 => 123.46
-  const itemsPrice = round2(
-    cartItems.reduce((a, c) => a + c.price * c.cartQuantity, 0)
-  );
-  console.log(itemsPrice)
-  const shippingPrice = itemsPrice > 200 ? 10 : 15;
-  const taxPrice = round2(itemsPrice * 0.15);
-  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
 
-  useEffect(() => {
-    if (!paymentMethod) {
-      router.push("/payment");
-    }
-    if (cartItems.length === 0) {
-      router.push("/cart");
-    }
-  }, []);
-  // const { closeSnackbar, enqueueSnackbar } = useSnackbar();
-  const [loading, setLoading] = useState(false);
-  const placeOrderHandler = async () => {
-    // closeSnackbar();
-    try {
-      setLoading(true);
-      //   const { data } = await axios.post(
-      //     '/api/orders',
-      //     {
-      //       orderItems: cartItems,
-      //       shippingAddress,
-      //       paymentMethod,
-      //       itemsPrice,
-      //       shippingPrice,
-      //       taxPrice,
-      //       totalPrice,
-      //     },
-      //     {
-      //       headers: {
-      //         authorization: `Bearer ${userInfo.token}`,
-      //       },
-      //     }
-      //   );
-      dispatch(clearBasket());
-      //Cookies.remove('cartItems');
-      localStorage.removeItem("cartItems");
-      setLoading(false);
-      //  router.push(`/order/${data._id}`);
-    } catch (err) {
-      setLoading(false);
-      //enqueueSnackbar(getError(err), { variant: 'error' });
-    }
-  };
+function PlaceOrder({shippingAddress,shippingPrice,taxPrice,totalPrice,placeOrderHandler,loading,classes,paymentMethod,cartItems,itemsPrice}) {
+  
   return (
     <>
       <CheckoutWizard activeStep={3}></CheckoutWizard>
--- pages/placeorder/index.js
+++ pages/placeorder/index.js
@@ -2,12 +2,83 @@ import React, { useState, useEffect } from "react";
 import PlaceOrder from "../../component/PlaceOrder";
 import { useRouter,withRouter } from 'next/router';
 
+import useStyles from "../../utils/styles";
+
+import { clearBasket } from "../../slice/basketSlice";
+//import Cookies from 'js-cookie';
+import { useSelector, useDispatch } from "react-redux";
+import localStorage from "localStorage";
 
 function placeorder() {
 
+  const classes = useStyles();
+  const router = useRouter();
+  const { dispatch } = useDispatch();
+  const {
+    cart: { cartItems, shippingAddress, paymentMethod },
+  } = useSelector(state => state.basket);
+  const round2 = num => Math.round(num * 100 + Number.EPSILON) / 100; // 123.456 => 123.46
+  const itemsPrice = round2(
+    cartItems.reduce((a, c) => a + c.price * c.cartQuantity, 0)
+  );
+  console.log(itemsPrice)
+  const shippingPrice = itemsPrice > 200 ? 10 : 15;
+  const taxPrice = round2(itemsPrice * 0.15);
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+
+  useEffect(() => {
+    if (!paymentMethod) {
+      router.push("/payment");
+    }
+    if (cartItems.length === 0) {
+      router.push("/cart");
+    }
+  }, []);
+  // const { closeSnackbar, enqueueSnackbar } = useSnackbar();
+  const [loading, setLoading] = useState(false);
+  const placeOrderHandler = async () => {
+    // closeSnackbar();
+    try {
+      setLoading(true);
+      //   const { data } = await axios.post(
+      //     '/api/orders',
+      //     {
+      //       orderItems: cartItems,
+      //       shippingAddress,
+      //       paymentMethod,
+      //       itemsPrice,
+      //       shippingPrice,
+      //       taxPrice,
+      //       totalPrice,
+      //     },
+      //     {
+      //       headers: {
+      //         authorization: `Bearer ${userInfo.token}`,
+      //       },
+      //     }
+      //   );
+      dispatch(clearBasket());
+      //Cookies.remove('cartItems');
+      localStorage.removeItem("cartItems");
+      setLoading(false);
+      //  router.push(`/order/${data._id}`);
+    } catch (err) {
+      setLoading(false);
+      //enqueueSnackbar(getError(err), { variant: 'error' });
+    }
+  };
   return (
     <PlaceOrder
-     
+      shippingAddress={shippingAddress}
+      shippingPrice={shippingPrice}
+      taxPrice={taxPrice}
+      totalPrice={totalPrice}
+      placeOrderHandler={placeOrderHandler}
+      loading={loading} 
+      classes={classes}
+      paymentMethod={paymentMethod}
+      cartItems={cartItems}
+      itemsPrice={itemsPrice}
     ></PlaceOrder>
   );
 }
