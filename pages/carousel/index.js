ecd4940a45f066eba8a04ef777dd4047c77d3a31
Usama <um3121997@gmail.com>
2022-06-27 18:08:47 -0500

    UI Settings

--- pages/carousel/index.js
+++ pages/carousel/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import CarouselApp from "../../container/Carousel/Carousel";
 import NewCarousel from "../../container/Card";
+import MediaCard from "../../container/Card/Card";
 
 const index = () => {
-  return <CarouselApp />;
+  return <MediaCard />;
   //   return <NewCarousel />;
 };
 
