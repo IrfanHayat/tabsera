5d278f29f69a1aad1759092c7fe36d880d374143
Irfan Hayat <irfanhayat55@gmail.com>
2022-07-25 12:03:50 +0500

    server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var https = require('https');
 var fs = require('fs');
 
 const next = require('next')
-const port = parseInt(process.env.PORT, 10) || config.getPort()
+//const port = parseInt(process.env.PORT, 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev, dir: __dirname })
 const handle = app.getRequestHandler()
