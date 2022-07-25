af27294b4edfa58bfb9c7661528fa734f193b35a
Irfan Hayat <irfanhayat55@gmail.com>
2022-07-25 11:59:26 +0500

    server

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const { createServer } = require('https');
-const { parse } = require('url');
-const next = require('next');
-const fs = require('fs');
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+var https = require('https');
+var fs = require('fs');
 
-const httpsOptions = {
+const next = require('next')
+const port = parseInt(process.env.PORT, 10) || config.getPort()
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev, dir: __dirname })
+const handle = app.getRequestHandler()
+
+
+var options = {
   key: fs.readFileSync('/etc/letsencrypt/live/tabsera.com/privkey3.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/tabsera.com/cert3.pem')
+   // ca: [fs.readFileSync('root.crt')]
 };
 
 app.prepare().then(() => {
-  createServer(httpsOptions, (req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-    
-  }).listen(3006, err => {
-    if (err) throw err;
-    console.log('> Ready on https://localhost:3006');
-  });
-});
+    https.createServer(options, (req, res) => {
+        // handle ....
+    }).listen(3006, err => {
+        if (err) throw err
+        console.log(`> Ready on localhost:${3006}`)
+    })
+})
