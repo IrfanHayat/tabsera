f20ce63477c41dc75813ca6dc21b8f8ae6f187ea
Irfan Hayat <irfanhayat55@gmail.com>
2022-07-25 11:43:45 +0500

    server.js

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-// const { createServer } = require('https');
-// const { parse } = require('url');
-// const next = require('next');
-// const fs = require('fs');
+const { createServer } = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
 
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-// const httpsOptions = {
-//   key: fs.readFileSync('/etc/letsencrypt/live/tabsera.com/privkey3.pem'),
-//   cert: fs.readFileSync('/etc/letsencrypt/live/tabsera.com/cert3.pem')
-// };
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/tabsera.com/privkey3.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/tabsera.com/cert3.pem')
+};
 
-// app.prepare().then(() => {
-//   createServer(httpsOptions, (req, res) => {
-//     const parsedUrl = parse(req.url, true);
-//     handle(req, res, parsedUrl);
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
     
-//   }).listen(3006, err => {
-//     if (err) throw err;
-//     console.log('> Ready on https://localhost:3006');
-//   });
-// });
+  }).listen(3006, err => {
+    if (err) throw err;
+    console.log('> Ready on https://localhost:3006');
+  });
+});
