c060796b8abf58b60aa2ec417c69e0e8c95afb87
Irfan Hayat <irfanhayat55@gmail.com>
2022-07-26 18:18:20 +0500

    server

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// const httpsOptions = {
-//     key: fs.readFileSync('/etc/letsencrypt/live/tijaricloud.com/self_signed/key.pem'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/tijaricloud.com/self_signed/cert.pem'),
-//     ca: [fs.readFileSync('/etc/letsencrypt/live/tijaricloud.com/self_signed/cert.pem')]
-// };
+const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/tijaricloud.com/self_signed/key.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/tijaricloud.com/self_signed/cert.pem'),
+    ca: [fs.readFileSync('/etc/letsencrypt/live/tijaricloud.com/self_signed/cert.pem')]
+};
 
 app.prepare().then(() => {
     createServer(httpsOptions, (req, res) => {
