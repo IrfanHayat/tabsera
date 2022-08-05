9d90e34ae165d694fbdb37aaf49b27b28af96175
Irfan Hayat <irfanhayat55@gmail.com>
2022-08-05 17:32:43 +0500

    shipping

--- container/DealsAndPromotions/MenuCards.js
+++ container/DealsAndPromotions/MenuCards.js
@@ -16,51 +16,69 @@ export default function MenuCard({
   console.log(discountsData);
   console.log(freeShippingData);
   return (
-    <div className="carousel-wrapper">
-      <Box
-        sx={{
-          mx: 4,
-          color: (theme) =>
-            theme.palette.mode === "dark" ? "grey.300" : "grey.800",
-          borderColor: (theme) =>
-            theme.palette.mode === "dark" ? "grey.800" : "grey.300",
-          fontSize: "1.3rem",
-          fontWeight: "700",
-        }}
-      >
-        {heading}
-      </Box>
-      <Carousel
-        // disableArrowsOnEnd={false}
-        showArrows={false}
-        pagination={false}
-        showEmptySlots={true}
-        itemsToScroll={1}
-        focusOnSelect={true}
-        // enableSwipe={true}
-        // enableMouseSwipe={true}
-        enableAutoPlay={true}
-        // autoPlaySpeed={2000}
-        // itemsToShow={2}
-        itemsToShow={3}
-      >
-        <Common
-          DealName="Deals"
-          address="deals_and_promotions"
-          imagePath="/sales.jpg"
-        />
-        <Common
-          DealName="Discounts"
-          address="discounts"
-          imagePath="/sales.jpg"
-        />
-        <Common
-          DealName="FreeShipping"
-          address="is_free_shipping"
-          imagePath="/freeshippings.jpg"
-        />
-        {/* <Common DealName="Coupons" address="coupons" /> */}
-      </Carousel>
-    </div>
+    <>
+      {console.log(discountsData.length)}
+      {console.log(dealsData.length)}
+      {console.log(freeShippingData?.length)}
+      {
+        freeShippingData?.length > 0 || dealsData.length > 0 || discountsData.length > 0 ?
+          <div className="carousel-wrapper">
+            <Box
+              sx={{
+                mx: 4,
+                color: (theme) =>
+                  theme.palette.mode === "dark" ? "grey.300" : "grey.800",
+                borderColor: (theme) =>
+                  theme.palette.mode === "dark" ? "grey.800" : "grey.300",
+                fontSize: "1.3rem",
+                fontWeight: "700",
+              }}
+            >
+              {heading}
+            </Box>
+            <Carousel
+              // disableArrowsOnEnd={false}
+              showArrows={false}
+              pagination={false}
+              showEmptySlots={true}
+              itemsToScroll={1}
+              focusOnSelect={true}
+              // enableSwipe={true}
+              // enableMouseSwipe={true}
+              enableAutoPlay={true}
+              // autoPlaySpeed={2000}
+              // itemsToShow={2}
+              itemsToShow={3}
+            >
+              {
+                dealsData ? <Common
+                  DealName="Deals"
+                  address="deals_and_promotions"
+                  imagePath="/sales.jpg"
+                /> : <></>
+              }
+
+              {
+                discountsData.length > 0 ? <Common
+                  DealName="Discounts"
+                  address="discounts"
+                  imagePath="/sales.jpg"
+                /> : <></>
+              }
+              {
+                freeShippingData.length > 0 ? <Common
+                  DealName="FreeShipping"
+                  address="is_free_shipping"
+                  imagePath="/freeshippings.jpg"
+                /> : <></>
+
+              }
+
+              {/* <Common DealName="Coupons" address="coupons" /> */}
+            </Carousel>
+          </div> : ''
+      }
+    </>
+
   );
 }
