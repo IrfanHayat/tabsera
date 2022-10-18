d2a43691a48ed4097fda6496c15b53b3a260e6bc
Irfan Hayat <irfanhayat55@gmail.com>
2022-10-18 18:02:21 +0500

    tabsera locker working

--- container/Locker/LockeDetails.js
+++ container/Locker/LockeDetails.js
@@ -1,6 +1,7 @@
 import { width } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
+import Radio from "@mui/material/Radio";
 import MapGL, { Marker } from "react-map-gl";
 import RoomIcon from "@mui/icons-material/Room";
 import Card from "@mui/material/Card";
@@ -14,6 +15,9 @@ import IconButton from "@mui/material/IconButton";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Box, Grid } from "@mui/material";
 import styles from "../../styles/locker.module.css";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 import AssistantDirectionOutlinedIcon from "@mui/icons-material/AssistantDirectionOutlined";
 import AssistantDirectionIcon from "@mui/icons-material/AssistantDirection";
 const TOKEN =
@@ -30,7 +34,7 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-const LockerDetails = ({ lockerData }) => {
+const LockerDetails = ({ handleChangeLocker, setRadioCheck1, lockerData }) => {
   const [expanded, setExpanded] = React.useState(-1);
   // const onSubscribeButton = async (id) => {
   //   try {
@@ -82,65 +86,101 @@ const LockerDetails = ({ lockerData }) => {
           item
           md={3}
           className={styles.lockerDetails}
-          // sx={{ overflowY: "scroll", height: 300 }}
+        // sx={{ overflowY: "scroll", height: 300 }}
         >
-          {lockerData?.map((locker, i) => (
-            <>
-              {/* <CardContent sx={{ padding: "0px 20px" }}> */}
-              {/* <Typography gutterBottom variant="h5" component="div">
+          <RadioGroup
+            // row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+            // value={labelValue}
+            onChange={handleChangeLocker}
+          >
+            {lockerData?.map((locker, i) => (
+              <>
+                {/* <CardContent sx={{ padding: "0px 20px" }}> */}
+                {/* <Typography gutterBottom variant="h5" component="div">
                   {locker.business_name}
                 </Typography> */}
-              <Box className={styles.lockerDetailsBox}>
-                <Typography variant="subtitle2">
-                  {locker.locker_name} Locker
-                </Typography>
-                <Typography variant="body2">
-                  Loation : {locker.locker_address}
-                </Typography>
-                <>
-                  <Button size="small" onClick={() => handleExpandClick(i)}>
-                    View Charge Plans
-                    <ExpandMore
-                      expand={expanded}
-                      aria-expanded={expanded}
-                      aria-label="show more"
-                    >
-                      <ExpandMoreIcon />
-                    </ExpandMore>
-                  </Button>
-                  <Divider fullWidth />
-                  {locker.isSubscribed ? (
-                    <Button variant="contained" disabled={true}>
-                      Subscribed
-                    </Button>
-                  ) : (
-                    <></>
-                    // <Button
-                    //   color="secondary"
-                    //   variant="contained"
-                    //   onClick={() => onSubscribeButton(lockerData.user_id)}
-                    // >
-                    //   Subscribe
-                    // </Button>
-                  )}
-                </>
-                <Collapse in={expanded === i} timeout="auto" unmountOnExit>
-                  <CardContent>
-                    {locker.charges.map((plans, i) => (
-                      <Typography paragraph className={styles.charges}>
-                        {plans.slot_size}
-                      </Typography>
-                    ))}
+                <Box className={styles.addressBoxDiv1}>
+                  <FormControlLabel
+                    value={locker.locker_id}
+                    control={<Radio />}
+                    label={
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        {/* <Image // className={cx(styles.media, mediaStyles.root)}
+                        src={"/locker_pic.jpg"}
+                        // onClick={(e) => viewCategory(product.category_id)}
+                        alt={"locker"}
+                        width={45}
+                        objectFit="contain"
+                        height={45}
+                      ></Image> */}
+                        <Typography
+                          sx={{
+                            p: 1,
+                          }}
+                        >
+                          {locker.locker_name}
+                        </Typography>
 
-                    {/* <Typography paragraph>charge plans here</Typography> */}
-                  </CardContent>
-                </Collapse>
-              </Box>
-              <AssistantDirectionOutlinedIcon
-                color="primary"
-                fontSize="large"
-              />
-              {/* <Typography variant="body2">
+
+                      </Box>
+                    }
+                    onClick={() => setRadioCheck1(true)}
+                  />
+
+                  <Typography variant="body2">
+                    Loation : {locker.locker_address}
+                  </Typography>
+                  <>
+                    <Button size="small" onClick={() => handleExpandClick(i)}>
+                      View Charge Plans
+                      <ExpandMore
+                        expand={expanded}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                      >
+                        <ExpandMoreIcon />
+                      </ExpandMore>
+                    </Button>
+                    <Divider fullWidth />
+                    {locker.isSubscribed ? (
+                      <Button variant="contained" disabled={true}>
+                        Subscribed
+                      </Button>
+                    ) : (
+                      <></>
+                      // <Button
+                      //   color="secondary"
+                      //   variant="contained"
+                      //   onClick={() => onSubscribeButton(lockerData.user_id)}
+                      // >
+                      //   Subscribe
+                      // </Button>
+                    )}
+                  </>
+                  <Collapse in={expanded === i} timeout="auto" unmountOnExit>
+                    <CardContent>
+                      {locker.charges.map((plans, i) => (
+                        <Typography paragraph className={styles.charges}>
+                          {plans.slot_size}
+                        </Typography>
+                      ))}
+
+                      {/* <Typography paragraph>charge plans here</Typography> */}
+                    </CardContent>
+                  </Collapse>
+                </Box>
+                <AssistantDirectionOutlinedIcon
+                  color="primary"
+                  fontSize="large"
+                />
+                {/* <Typography variant="body2">
                   Available Slots of Lockers: {locker.noOfLockerAvailableSlots}
                 </Typography>
                 <Typography variant="body2">
@@ -149,9 +189,10 @@ const LockerDetails = ({ lockerData }) => {
                 <Typography variant="body2">
                   Available Slots in Vending Machines: {locker.noOfVendingMachineAvailableSlots}
                 </Typography> */}
-              {/* </CardContent> */}
-            </>
-          ))}
+                {/* </CardContent> */}
+              </>
+            ))}
+          </RadioGroup>
         </Grid>
         <Grid item md={9}>
           {/* hello */}
